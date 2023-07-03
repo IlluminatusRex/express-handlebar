035c3d4f2b86def60d48e7529fc168cada18f176
Jakub Czesak <jakub.czesak@cbre.com>
2023-07-03 23:33:39 +0200

    Add Contact form

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
 const app = express();
+
+const upload = multer();
 
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '/public')));
 
@@ -32,7 +37,19 @@ app.get('/hello/:name', (req, res) => {
   res.render('hello', { name: req.params.name });
 });
 
+app.post('/contact/send-message', upload.single('file'), (req, res) => {
+  
+  const { author, sender, title, message } = req.body;
+  const file = req.file;
 
+  if(author && sender && title && message && file) {
+    res.render('contact', { isSent: true, fileName: file.originalname  });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
+
+});
 
 
 
